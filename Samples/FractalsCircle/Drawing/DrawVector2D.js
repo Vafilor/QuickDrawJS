b6f58d1badc51170131be3e7d467f069f474356b
Andrey Melnikov <andreypmelnikov@eagles.ewu.edu>
2015-06-04 21:35:16 -0700

    Removed script copies and changed the imported scripts to use the general models/drawing ones.

--- Samples/FractalsCircle/Drawing/DrawVector2D.js
+++
@@ -1,9 +0,0 @@
-function DrawVector2D()
-{
-}
-
-function drawVector2DPoint(vector, size, color) 
-{
-	Graphics.drawFilledCircle(drawingInformation.brush, drawingInformation.xOffset + vector.x,
-									 drawingInformation.yOffset + vector.y, size, color);
-}
